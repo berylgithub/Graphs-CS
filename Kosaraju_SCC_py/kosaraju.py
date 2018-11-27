3c83cfef815eff7374faba934afed36bd8597959
DESKTOP-QTCAKUD\YorozuyaSaint <berylramadhian@gmail.com>
2018-11-27 20:51:29 +0700

    Completed Kosaraju Algorithm to find strongly connected component in graph (SCC)

--- Kosaraju_SCC_py/kosaraju.py
+++ Kosaraju_SCC_py/kosaraju.py
@@ -50,42 +50,49 @@ def kosaraju_DFS(graph, stack, visited, v_kosaraju, log=True):
     
     return v_kosaraju
 
-def kosaraju_reverse_DFS(graph, stack, visited, scc, log=True):
+def kosaraju_reverse_DFS(reversed_graph, stack, visited, kos_vector, kos_track, scc, log=True):
     if log :
-        print("stack = ",stack, "track = ",visited)
+        print("stack = ",stack, "track = ",visited, "kos_vector=",kos_vector, "kos_track=",kos_track, "scc=",scc)
     #check if stack is empty
     if not stack:   
-        #if there are unvisited nodes, do dfs
-        if len(visited)<len(graph):
-            for node in graph:
+        #if the the kosaraju vector is not empty, visit the next node
+        if kos_vector:
+            for node in kos_vector:
                 if node not in visited:
                     stack += [node]
                     visited += [node]
+                    kos_track += [node]
+                    kos_vector.remove(node)
                     break
-            kosaraju_reverse_DFS(graph, stack, visited, scc, log)
-        #if all of the graph's node has been visited, just print it
-        elif len(visited)==len(graph):
+            kosaraju_reverse_DFS(reversed_graph, stack, visited, kos_vector, kos_track, scc, log)
+        #if kosaraju vector is empty
+        elif not kos_vector:
             if log:
-                print("DFS finished")
-                print("track = ",visited)
+                print("reversed graph DFS finished")
+                print("Strongly Connected Components = ", scc)
             
     else:
         #get stack top
         current = stack[-1]
         #traverse stack top's child
-        for child in graph[current]:
-            if child not in visited:
-                stack += [child]
-                visited += [child]
-                break
+        if current in reversed_graph:
+            for child in reversed_graph[current]:
+                if child not in visited:
+                    stack += [child]
+                    visited += [child]
+                    kos_track += [child]
+                    kos_vector.remove(child)
+                    break
             
-        #if the stack's top doesnt change, add the current route to SCC list, then pop all of the routes from kosaraju vector
+        #if the stack's top doesnt change, means it is backtrack time, means append kos_track to scc, then empty kos_track
         if current == stack[-1]:
-            scc.append(visited)
+            #check if kos_track is empty, if yes, do nothing, else append it to scc list
+            if kos_track:
+                scc.append(kos_track)
+                kos_track = []
             stack = stack[0:-1]
-        kosaraju_reverse_DFS(graph, stack, visited, scc, log)
+        kosaraju_reverse_DFS(reversed_graph, stack, visited, kos_vector, kos_track, scc, log)
     
-    return scc
 
 
 def kosaraju_helper(graph, root, log=True):
@@ -120,27 +127,41 @@ def kosaraju_helper(graph, root, log=True):
                 reversed_graph[child] += [parent]
     print("reversed graph = ",reversed_graph)
     
-    #traverse (again?...) the reversed graph to get the strongly connected components
-    stack = [reversed_graph[0]]
-    visited = [reversed_graph[0]]
+    #traverse (again?...) the reversed graph to get the strongly connected components, start node by referring to kosaraju vector
+    kos_vector = v_kosaraju_final
+    root = kos_vector[0]
+    stack = [root]
+    visited = [root]
+    kos_track = [root]
+    kos_vector.remove(root)
     scc = []
     
     #start of DFS
-    for child in graph[root]:
-        if child not in visited:
-            stack += [child]
-            visited += [child]
-            break
-    #get the kosaraju vector using dfs
-    #kosaraju_DFS(graph, stack, visited, v_kosaraju, log)
+    if root in reversed_graph:
+        for child in reversed_graph[root]:
+            if child not in visited:
+                stack += [child]
+                visited += [child]
+                kos_track += [child]
+                kos_vector.remove(child)
+                break
+    kosaraju_reverse_DFS(reversed_graph, stack, visited, kos_vector, kos_track, scc, log)
     
     
 if __name__ == "__main__":
+    print("Example 1 : ")
     graph = {0:[2,3], 1:[0], 2:[1], 3:[4], 4:[]}
     kosaraju_helper(graph, 0)
     
-
-
+    print("\nExample 2 :")
+    graph = {"A":["B"], "B":["C","D"], "C":["A"], "D":["E"],
+             "E":["F"], "F":["D"], "G":["F","H"], "H":["I"],
+             "I":["J"], "J":["G","K"], "K":[]}
+    kosaraju_helper(graph, "A")
+    
+    print("\nExample 3 :")
+    graph = {0:[1], 1:[2], 2:[3,4], 3:[0], 4:[5], 5:[6], 6:[4], 7:[6,8], 8:[]}
+    kosaraju_helper(graph, 0)
     
 
     
